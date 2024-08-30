b58006ba260b494cff050e9580ebd3f8651da030
rubnx <rubns.ruiz@gmail.com>
2024-08-30 17:29:33 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import initialTasks from './data/initialTasks.js';
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Form tasks={tasks} onTasks={setTasks} />
+      <Content tasks={tasks} onTasks={setTasks} />
+      <Footer />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>
+        <span>☑️</span> Stuff To Do
+      </h1>
+    </div>
+  );
+}
+
+function Form({ onTasks }) {
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newTask = {
+      id: crypto.randomUUID(),
+      description,
+      finished: false,
+    };
+    console.log(newTask);
+    onTasks((tasks) => [...tasks, newTask]);
+
+    setDescription(''); // Clear form after submit
+  }
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <input
+        placeholder='Add a new task...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button className='form-btn'>+</button>
+    </form>
+  );
+}
+
+function Content({ tasks, onTasks }) {
+  function toggleTask(id) {
+    onTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, finished: !task.finished } : task
+      )
+    );
+  }
+
+  function deleteTask(id) {
+    onTasks((tasks) => tasks.filter((task) => task.id !== id));
+  }
+
+  return (
+    <div className='content'>
+      <UnfinishedTaskList
+        tasks={tasks}
+        onToggleTask={toggleTask}
+        onDeleteTask={deleteTask}
+      />
+      <FinishedTaskList
+        tasks={tasks}
+        onToggleTask={toggleTask}
+        onDeleteTask={deleteTask}
+      />
+    </div>
+  );
+}
+
+function UnfinishedTaskList({ tasks, onToggleTask, onDeleteTask }) {
+  return (
+    <>
+      {tasks.filter((task) => task.finished === false).length > 0 ? (
+        <div className='unfinished-task-list'>
+          <ul className='tasks'>
+            {tasks
+              .filter((task) => task.finished === false)
+              .map((task) => (
+                <UnfinishedTask
+                  task={task}
+                  onToggleTask={onToggleTask}
+                  onDeleteTask={onDeleteTask}
+                  key={task.id}
+                />
+              ))}
+          </ul>
+        </div>
+      ) : (
+        <h1 className='message'>
+          <span>😅</span>
+          Nothing to see here, add a new task
+        </h1>
+      )}
+    </>
+  );
+}
+
+function FinishedTaskList({ tasks, onToggleTask, onDeleteTask }) {
+  return (
+    <div className='finished-task-list'>
+      <ul className='tasks'>
+        {tasks
+          .filter((task) => task.finished === true)
+          .map((task) => (
+            <FinishedTask
+              task={task}
+              onToggleTask={onToggleTask}
+              onDeleteTask={onDeleteTask}
+              key={task.id}
+            />
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+function UnfinishedTask({ task, onToggleTask, onDeleteTask }) {
+  return (
+    <li className='unfinished-task'>
+      <div className='task-checkbox'>
+        <input
+          type='checkbox'
+          checked={task.finished}
+          onChange={() => onToggleTask(task.id)}
+        ></input>
+        <label>{task.description}</label>
+      </div>
+      <button className='delete-btn' onClick={() => onDeleteTask(task.id)}>
+        ❌
+      </button>
+    </li>
+  );
+}
+
+function FinishedTask({ task, onToggleTask, onDeleteTask }) {
+  return (
+    <li className='finished-task'>
+      <div className='task-checkbox'>
+        <input
+          type='checkbox'
+          checked={task.finished}
+          onChange={() => onToggleTask(task.id)}
+        ></input>
+        <label>{task.description}</label>
+      </div>
+      <button className='delete-btn' onClick={() => onDeleteTask(task.id)}>
+        ❌
+      </button>
+    </li>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      © {new Date().getFullYear()} Nibelheim, Inc.
+    </footer>
   );
 }
 
---
+++ src/data/initialTasks.js
@@ -0,0 +1,30 @@
+// Initial Test Data
+const initialTasks = [
+  {
+    id: '1',
+    description: 'Complete the project proposal',
+    finished: false,
+  },
+  {
+    id: '2',
+    description: 'Search for simple coding projects',
+    finished: false,
+  },
+  {
+    id: '3',
+    description: 'Start ToDo list project',
+    finished: true,
+  },
+  {
+    id: '4',
+    description: 'Test new app',
+    finished: true,
+  },
+  {
+    id: '5',
+    description: 'Fix app bugs',
+    finished: false,
+  },
+];
+
+export default initialTasks;
